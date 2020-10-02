3f38f34b8f8df9d0ad0aea4cdd548314e07a0e81
JesusDiaz08 <jesus_kdm@hotmail.com>
2020-10-02 14:48:07 -0500

    archivos.h | Solo definiciones

--- 02 Seno2/archivos.h
+++ 02 Seno2/archivos.h
@@ -1,7 +1,7 @@
 /**
 * @brief: archivos.h
 **/
-#ifndef ARCHIVOS_H
+#ifndef ARCHIVOS_H   // Si no esta definido ARCHIVOS_H, definelo y se compile una sola vez
 #define ARCHIVOS_H
 
 void guardarDatos(float datos[]); 
